1c378dd3aab01093ec4ced0420791bd732ae6392
Batuhan Taluk <wtaluk2002@gmail.com>
2025-10-23 12:59:37 +0300

    publish için json güncellemesi

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const REPO_NAME = "mscolorwebsite";
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: `\${REPO_NAME}`, 
 })
