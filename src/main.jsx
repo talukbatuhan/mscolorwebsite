6dd9bb71fc3fd0419dd6b419fd9177b238a58d4d
Batuhan Taluk <wtaluk2002@gmail.com>
2025-10-23 13:15:23 +0300

    fix: Removed GitHub Pages base path configs for Vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'; // 👈 BrowserRouter'ı içeri aktardık
+import { BrowserRouter } from 'react-router-dom'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* Uygulamayı yönlendirme özelliği ile sarmalıyoruz */}
-    <BrowserRouter basename="/mscolorwebsite">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
