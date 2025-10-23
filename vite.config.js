e5a2e4463f7a8bef4f3087907dd471de8b238a88
Batuhan Taluk <wtaluk2002@gmail.com>
2025-10-23 12:54:43 +0300

    publish etmek için güncelleme

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // KRİTİK AYAR: Yayınlamak için depo adınızı buraya ekliyoruz.
-  // URL'niz: https://[Kullanıcı Adınız].github.io/mscolorweb/
   base: '/', 
 })
